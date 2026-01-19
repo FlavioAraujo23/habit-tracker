1334c9884a71a01de6009ce5211dc232a2179593
flavio <flaviolopes1027@gmail.com>
2026-01-19 16:44:53 -0300

    implement user authentication with login form

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,10 +4,33 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         return view('login');
+    }
+
+    public function authenticate(Request $request)
+    {
+        $credentials = $request->validate(([
+            'email' => 'required|email',
+            'password' => 'required|min:6',
+        ]));
+
+        if (Auth::attempt($credentials)) {
+            // se o usuario estiver autenticado, regenera a sessao
+            $request->session()->regenerate();
+
+            // envia ele para home
+            return redirect()->intended('/dashboard');
+        } else {
+            // se as credenciais forem invalidas, retorna com erro
+            return back()->withErrors([
+                'email' => 'Credenciais inválidas.',
+            ]);
+        }
     }
 }
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,5 +1,17 @@
 <x-layout>
     <main class="py-10">
         <h1>Veja seus hábitos ganharem vida</h1>
+        <section>
+            <div>
+                @error('email')
+                    <p class="text-red-500 mt-2">{{ $message }}</p>
+                @enderror
+            </div>
+            <form action="/login" method="POST" class="flex flex-col gap-4">
+                @csrf
+                <input type="email" name="email" placeholder="your@email.com" class="bg-white p-2 border-2">
+                <input type="password" name="password" placeholder="*********" class="bg-white p-2 border-2">
+                <button type="submit" class="bg-white p-2 border-2">Login</button>
+            </form>
     </main>
 </x-layout>
